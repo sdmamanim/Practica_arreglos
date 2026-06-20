f8920094b424157ae68bf95a48d1fd301130641e
sdmamanim <sdmamanim@unjbg.edu.pe>
2026-06-19 20:03:52 -0500

    programa terminado

--- reverso.cpp
+++ reverso.cpp
@@ -4,4 +4,16 @@ int main(){
 	int n, A[100];
 	cout<<"ingrese la cantidad de elementos: ";
 	cin>>n;
+    	
+	for(int i=0; i<n; i++){
+	 	
+		 cin>>A[i];
+		 
+	}
+	
+	for(int i=n-1; i>=0; i--){
+	 	
+		 cout<<A[i]<<" ";
+	 }
+	return 0;
 }
