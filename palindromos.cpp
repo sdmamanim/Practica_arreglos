becd1e3624d45b03edf51140de88b2a043ceec70
sdmamanim <sdmamanim@unjbg.edu.pe>
2026-06-20 15:20:24 -0500

    completamos la funcion

--- palindromos.cpp
+++ palindromos.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 bool comprobarPalindromo(string palabra ){
 
+    int izquierda = 0;
+    int derecha = palabra.length() - 1;
+    
+    while( izquierda < derecha ){
+
+        if( palabra[izquierda] != palabra[derecha] ){
+            return false;
+
+        }
+        izquierda = izquierda + 1;
+        derecha = derecha - 1;
+    }
+    return true;
 }
 
 
