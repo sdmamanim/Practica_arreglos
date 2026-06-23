2c32a6f509459e3f482bb60ce4b81f2a8e58e2e3
sdmamanim <sdmamanim@unjbg.edu.pe>
2026-06-22 23:14:09 -0500

    se hara la recoleccion de los datos y finalmente se realizaran las salidas

--- canicas.cpp
+++ canicas.cpp
@@ -2,10 +2,23 @@
 using namespace std;
 
 int main(){
-    int N, C;
+    int N, C, destino ;
     cin>>N>>C;
 
     int vasos[1000] = {0};
 
+    while( C>0 ){
+        C = C-1;
+        cin>>destino;
 
+        if(destino>=1 && destino<=N ){
+            vasos[destino]++;
+        }
+    }
+
+    for( int i=1; i<=N; i++){
+        cout<<vasos[i]<<endl;
+
+    }
+    return 0;
 }
